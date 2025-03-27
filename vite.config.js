e414752886d15dbcac74351aaffcc091e0e6feb1
Thomas <telcontar@hotmail.co.uk>
2025-03-27 21:17:25 +0000

    Setup vite project. Added basic site framework. Started implementation of Bestiary page sidebar.

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './', 
+    server:
+    {
+        host: true,
+    },
+    build:
+    {
+        outDir: '../docs',
+        emptyOutDir: true,
+        sourcemap: true
+    }
+}
