f1c92e05bade823663d224b7d00ce049458dd59d
Thomas <telcontar@hotmail.co.uk>
2025-03-29 16:59:57 +0000

    Fixed offcanvas returning to top of page when closing the offcanvas. Moved offcanvas toggle button to the navbar. Added Kurale font to bestiary entries. Minor style changes to bestiary. Begun setting up a bestiary sources file.

---
+++ bestiarySource.js
@@ -0,0 +1,47 @@
+export default
+{
+    categories: 
+    {
+        cursed: "Cursed Ones",
+        draconids: "Draconids",
+        necrophages: "Necrophages",
+        relicts: "Relicts"
+    },
+
+    entries: 
+    {
+        template: 
+        {
+            name: "Template",
+            image: "",
+            category: categories.cursed,
+            size: "Medium",
+            overview: "",
+            strengths: "",
+            weaknesses: "",
+            peculiarities: "",
+            lures: "",
+            harvesting: "",
+            about: ""
+        },
+        werewolf: 
+        {
+            name: "Werewolf",
+            image: "",
+            category: categories.cursed,
+            size: "Medium",
+            overview: "",
+            strengths: "",
+            weaknesses: "",
+            peculiarities: "",
+            lures: "",
+            harvesting: "",
+            about: ""
+        },
+        wyvern:
+        {
+
+        }
+    }
+
+}
