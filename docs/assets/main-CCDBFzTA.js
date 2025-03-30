a1ad143a50d76d2db64fdbdd0e465f6fd1deb8ff
Thomas <telcontar@hotmail.co.uk>
2025-03-30 03:16:19 +0100

    Build.

---
+++ docs/assets/main-CCDBFzTA.js
@@ -0,0 +1,2 @@
+import"./bootstrap.esm-BZO0LS4N.js";console.log("JavaScript working!");
+//# sourceMappingURL=main-CCDBFzTA.js.map
--- docs/assets/main-DIpQttrB.js
+++
@@ -1,2 +0,0 @@
-import"./bootstrap.esm-Dcf93i-y.js";console.log("JavaScript working!");
-//# sourceMappingURL=main-DIpQttrB.js.map
