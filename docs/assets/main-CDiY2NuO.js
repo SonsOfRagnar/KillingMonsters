83f31bd9ced77c277c951e0f612ff8baf38d52d4
Thomas <telcontar@hotmail.co.uk>
2025-06-05 16:04:57 +0100

    Build site

--- docs/assets/main-CDiY2NuO.js
+++
@@ -1,2 +0,0 @@
-import"./bootstrap.esm-DeLdebHC.js";console.log("JavaScript working!");
-//# sourceMappingURL=main-CDiY2NuO.js.map
---
+++ docs/assets/main-DKnRfGpL.js
@@ -0,0 +1,2 @@
+import"./bootstrap.esm-C-XZIA4p.js";console.log("JavaScript working!");
+//# sourceMappingURL=main-DKnRfGpL.js.map
